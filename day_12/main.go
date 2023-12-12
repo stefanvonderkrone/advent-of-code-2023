f1d3f0b27421b9d61a555766a583f859f63daa66
Stefan von der Krone <stefan.krone@check24.de>
2023-12-12 21:53:04 +0100

    day 12 pt2 suckz

--- day_12/main.go
+++ day_12/main.go
@@ -5,24 +5,56 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
+
+type Order []byte
+
+type Cache struct {
+    cache map[string]bool
+}
+
+func newCache() Cache {
+    return Cache{cache: map[string]bool{}}
+}
+
+func (c *Cache) add(key string, value bool) {
+    c.cache[key] = value
+}
+
+func (c *Cache) get(key string) (bool, bool) {
+    v, ok := c.cache[key]
+    return v, ok
+}
 
 type Arrangement struct {
     Order []byte
     UnknownIndices []int
-    Amounts []int
+    Amounts *[]int
+    SumAmounts int
+    Cache *Cache
+}
+
+func getUnknownIndices(order []byte) []int {
+    u := []int{}
+    for i, char := range order {
+        if char == '?' {
+            u = append(u, i)
+        }
+    }
+    return u
 }
 
 func readLine(line string) Arrangement {
     i := 0
-    order := []byte{}
-    unknownIndices := []int{}
     for line[i] != ' ' {
-        order = append(order, line[i])
-        if line[i] == '?' {
-            unknownIndices = append(unknownIndices, i)
-        }
         i++
+    }
+    order := []byte(line[0:i])
+    unknownIndices := getUnknownIndices(order)
+    fmt.Printf("%+v\n", unknownIndices)
+    for _, index := range unknownIndices {
+        order[index] = '.'
     }
     i++
     k := i
@@ -37,7 +69,12 @@ func readLine(line string) Arrangement {
         }
         i++
     }
-    return Arrangement{Order: order, UnknownIndices: unknownIndices, Amounts: amounts}
+    cache := newCache()
+    sumAmounts := 0
+    for _, a := range amounts {
+        sumAmounts += a
+    }
+    return Arrangement{Order: order, UnknownIndices: unknownIndices, Amounts: &amounts, SumAmounts: sumAmounts, Cache: &cache}
 }
 
 type BitArrangment struct {
@@ -57,6 +94,7 @@ func (b *BitArrangment) Next() bool {
     }
     b.current = b.next
     b.next += 1
+    // fmt.Printf("%d/%d\n", b.next, b.target)
     return b.current < b.target
 }
 
@@ -115,7 +153,34 @@ func (a *Arrangement) isValidOrder(order []byte) bool {
     }
     // fmt.Printf("%s, %+v\n", string(order), amounts)
     // fmt.Printf("%+v, %+v, %+v, %t\n", order,  amounts, a.Amounts, intsEqual(amounts, a.Amounts))
-    return intsEqual(amounts, a.Amounts)
+    return intsEqual(amounts, *a.Amounts)
+}
+
+func (a *Arrangement) isValid() bool {
+    amount := 0
+    sumAmounts := 0
+    numAmounts := 0
+    i := 0
+    amounts := *a.Amounts
+    for _, char := range a.Order {
+        if char == '#' {
+            amount++
+        } else if amount > 0 {
+            if amounts[i] != amount {
+                return false
+            }
+            sumAmounts += amount
+            if sumAmounts > a.SumAmounts {
+                return false
+            }
+            numAmounts++
+            if numAmounts > len(amounts) {
+                return false
+            }
+            amount = 0
+        }
+    }
+    return true
 }
 
 func (a *Arrangement) countPossibleArrangements() int {
@@ -143,17 +208,71 @@ func (a *Arrangement) countPossibleArrangements() int {
     return sum
 }
 
+func buildTree(a Arrangement) int {
+    if len(a.UnknownIndices) == 0 {
+        if _, ok := a.Cache.get(string(a.Order)); ok {
+            return 0
+        }
+        // fmt.Printf("%s, %+v\n", string(a.Order), *a.Amounts)
+        if a.isValidOrder(a.Order) {
+            return 1
+        }
+        return 0
+    }
+    u := a.UnknownIndices[0]
+    unknowns := a.UnknownIndices[1:]
+    order := a.Order
+    // fmt.Printf("%s\n", string(order))
+    order[u] = '#'
+    left := buildTree(Arrangement{order, unknowns, a.Amounts, a.SumAmounts, a.Cache})
+    order[u] = '.'
+    right := buildTree(Arrangement{order, unknowns, a.Amounts, a.SumAmounts, a.Cache})
+    return left + right
+}
+
+func (a *Arrangement) countTree() int {
+    return buildTree(*a)
+}
+
+func (a *Arrangement) unfold(count int) {
+    // fmt.Printf("%s\n", string(a.Order))
+    order := a.Order
+    amounts := *a.Amounts
+    unknownIndices := a.UnknownIndices
+    for count > 0 {
+        orderLength := len(a.Order)
+        newUnknowns := append(a.UnknownIndices, orderLength)
+        for _, u := range unknownIndices {
+            newUnknowns = append(newUnknowns, u + orderLength + 1)
+        }
+        a.UnknownIndices = newUnknowns
+        a.Order = append(a.Order, '?')
+        a.Order = append(a.Order, order...)
+        newAmounts := append(*a.Amounts, amounts...)
+        a.Amounts = &newAmounts
+        count--
+    }
+    // a.UnknownIndices = getUnknownIndices(a.Order)
+    // fmt.Printf("%s\n", string(a.Order))
+}
+
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
 
     sum := 0
+    start := time.Now()
     for scanner.Scan() {
         line := scanner.Text()
         // fmt.Print(line)
         arrangement := readLine(line)
-        sum += arrangement.countPossibleArrangements()
+        arrangement.unfold(3)
+        fmt.Printf("%+v\n",arrangement)
+        sum += arrangement.countTree()
+        // sum += arrangement.countPossibleArrangements()
         //fmt.Printf("%+v\n", arrangement)
     }
+    end := time.Since(start)
     fmt.Printf("%d\n", sum)
+    fmt.Printf("took %s\n", end)
 
 }
