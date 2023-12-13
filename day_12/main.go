c5164b5b54f0c5cebfce4ca69bf767a1bbd0d1ae
Stefan von der Krone <stefan.krone@check24.de>
2023-12-13 09:38:54 +0100

    day 12 pt1 again

--- day_12/main.go
+++ day_12/main.go
@@ -5,57 +5,16 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-type Order []byte
-
-type Cache struct {
-    cache map[string]bool
-}
-
-func newCache() Cache {
-    return Cache{cache: map[string]bool{}}
-}
-
-func (c *Cache) add(key string, value bool) {
-    c.cache[key] = value
-}
-
-func (c *Cache) get(key string) (bool, bool) {
-    v, ok := c.cache[key]
-    return v, ok
-}
-
-type Arrangement struct {
-    Order []byte
-    UnknownIndices []int
-    Amounts *[]int
-    SumAmounts int
-    Cache *Cache
-}
-
-func getUnknownIndices(order []byte) []int {
-    u := []int{}
-    for i, char := range order {
-        if char == '?' {
-            u = append(u, i)
-        }
-    }
-    return u
-}
-
-func readLine(line string) Arrangement {
+func readLine(line string) (string, []int) {
     i := 0
     for line[i] != ' ' {
         i++
     }
-    order := []byte(line[0:i])
-    unknownIndices := getUnknownIndices(order)
-    fmt.Printf("%+v\n", unknownIndices)
-    for _, index := range unknownIndices {
-        order[index] = '.'
-    }
+    order := line[0:i]
     i++
     k := i
     amounts := []int{}
@@ -69,191 +28,48 @@ func readLine(line string) Arrangement {
         }
         i++
     }
-    cache := newCache()
-    sumAmounts := 0
-    for _, a := range amounts {
-        sumAmounts += a
-    }
-    return Arrangement{Order: order, UnknownIndices: unknownIndices, Amounts: &amounts, SumAmounts: sumAmounts, Cache: &cache}
+    return order, amounts
 }
 
-type BitArrangment struct {
-    length int
-    target int
-    current int
-    next int
-}
-
-func newBitArrangment(length int) BitArrangment {
-    return BitArrangment{length: length, target: 1 << length, current: 0, next: 0}
-}
-
-func (b *BitArrangment) Next() bool {
-    if b.current == b.target {
-        return false
-    }
-    b.current = b.next
-    b.next += 1
-    // fmt.Printf("%d/%d\n", b.next, b.target)
-    return b.current < b.target
-}
-
-func (b *BitArrangment) Get() []int {
-    arrangement := make([]int, b.length)
-    bitString := strconv.FormatInt(int64(b.current), 2)
-    numBits := len(bitString)
-    i := numBits
-    k := 0
-    for i > 0 {
-        i--
-        if bitString[i] == '1' {
-            arrangement[k] = 1
-        }
-        k++
-    }
-    return arrangement
-}
-
-// ???
-// ...
-// #..
-// ##.
-// #.#
-// ###
-// .#.
-// .##
-// ..#
-
-func intsEqual(a []int, b []int) bool {
-    if len(a) != len(b) {
-        return false
-    }
-    for i, v := range a {
-        if v != b[i] {
-            return false
-        }
-    }
-    return true
-}
-
-func (a *Arrangement) isValidOrder(order []byte) bool {
-    amount := 0
-    amounts := []int{}
-    for _, char := range order {
-        if char == '#' {
-            amount++
-        } else if amount > 0 {
-            amounts = append(amounts, amount)
-            amount = 0
-        }
-        // fmt.Printf("%d, %d\n", char, amount)
-    }
-    if amount > 0 {
-        amounts = append(amounts, amount)
-    }
-    // fmt.Printf("%s, %+v\n", string(order), amounts)
-    // fmt.Printf("%+v, %+v, %+v, %t\n", order,  amounts, a.Amounts, intsEqual(amounts, a.Amounts))
-    return intsEqual(amounts, *a.Amounts)
-}
-
-func (a *Arrangement) isValid() bool {
-    amount := 0
-    sumAmounts := 0
-    numAmounts := 0
-    i := 0
-    amounts := *a.Amounts
-    for _, char := range a.Order {
-        if char == '#' {
-            amount++
-        } else if amount > 0 {
-            if amounts[i] != amount {
-                return false
-            }
-            sumAmounts += amount
-            if sumAmounts > a.SumAmounts {
-                return false
-            }
-            numAmounts++
-            if numAmounts > len(amounts) {
-                return false
-            }
-            amount = 0
-        }
-    }
-    return true
-}
-
-func (a *Arrangement) countPossibleArrangements() int {
-    bitArragement := newBitArrangment(len(a.UnknownIndices))
-    sum := 0
-    for bitArragement.Next() {
-        bits := bitArragement.Get()
-        order := a.Order
-        for i, bit := range bits {
-            sign := byte(0)
-            if bit == 1 {
-                sign = '#'
-            } else {
-                sign = '.'
-            }
-            index := a.UnknownIndices[i]
-            order[index] = sign
-        }
-        if a.isValidOrder(order) {
-            sum++
-        }
-        // fmt.Printf("%s\n", string(order))
-    }
-    // fmt.Printf("%d\n", sum)
-    return sum
-}
-
-func buildTree(a Arrangement) int {
-    if len(a.UnknownIndices) == 0 {
-        if _, ok := a.Cache.get(string(a.Order)); ok {
-            return 0
-        }
-        // fmt.Printf("%s, %+v\n", string(a.Order), *a.Amounts)
-        if a.isValidOrder(a.Order) {
+func count(cfg string, nums []int) int {
+    if cfg == "" {
+        if len(nums) == 0 {
             return 1
         }
         return 0
     }
-    u := a.UnknownIndices[0]
-    unknowns := a.UnknownIndices[1:]
-    order := a.Order
-    // fmt.Printf("%s\n", string(order))
-    order[u] = '#'
-    left := buildTree(Arrangement{order, unknowns, a.Amounts, a.SumAmounts, a.Cache})
-    order[u] = '.'
-    right := buildTree(Arrangement{order, unknowns, a.Amounts, a.SumAmounts, a.Cache})
-    return left + right
-}
 
-func (a *Arrangement) countTree() int {
-    return buildTree(*a)
-}
-
-func (a *Arrangement) unfold(count int) {
-    // fmt.Printf("%s\n", string(a.Order))
-    order := a.Order
-    amounts := *a.Amounts
-    unknownIndices := a.UnknownIndices
-    for count > 0 {
-        orderLength := len(a.Order)
-        newUnknowns := append(a.UnknownIndices, orderLength)
-        for _, u := range unknownIndices {
-            newUnknowns = append(newUnknowns, u + orderLength + 1)
+    if len(nums) == 0 {
+        if strings.Contains(cfg, "#") {
+            return 0
         }
-        a.UnknownIndices = newUnknowns
-        a.Order = append(a.Order, '?')
-        a.Order = append(a.Order, order...)
-        newAmounts := append(*a.Amounts, amounts...)
-        a.Amounts = &newAmounts
-        count--
+        return 1
     }
-    // a.UnknownIndices = getUnknownIndices(a.Order)
-    // fmt.Printf("%s\n", string(a.Order))
+
+    result := 0
+
+    if cfg[0] == '.' || cfg[0] == '?' {
+        result += count(cfg[1:], nums)
+    }
+
+    if cfg[0] == '#' || cfg[0] == '?' {
+        if nums[0] > len(cfg) {
+            return result
+        }
+        if strings.Contains(cfg[:nums[0]], ".") {
+            return result
+        }
+
+        if nums[0] == len(cfg) || cfg[nums[0]] != '#' {
+            startIndex := nums[0] + 1
+            if startIndex > len(cfg) {
+                startIndex = len(cfg)
+            }
+            result += count(cfg[startIndex:], nums[1:])
+        }
+    }
+
+    return result
 }
 
 func main() {
@@ -264,10 +80,8 @@ func main() {
     for scanner.Scan() {
         line := scanner.Text()
         // fmt.Print(line)
-        arrangement := readLine(line)
-        arrangement.unfold(3)
-        fmt.Printf("%+v\n",arrangement)
-        sum += arrangement.countTree()
+        cfg, nums := readLine(line)
+        sum += count(cfg, nums)
         // sum += arrangement.countPossibleArrangements()
         //fmt.Printf("%+v\n", arrangement)
     }
